853b5c1a2ff0d84d2d11aabfb5beb42d96962fde
Dattor <Dattor@github.com>
2024-01-22 21:54:09 +0100

    printf

---
+++ printf/printf.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void put_string(char *str, int *len)
+{
+	if (!str)
+		str = "(null)";
+	while(*str)
+		*len += write(1, str++, 1);
+}
+
+void put_digit(long long int number, int base, int *len)
+{
+	char *cipher = "0123456789abcdef";
+	if(number < 0)
+	{
+		number *= -1;
+		*len += write(1,"-",1);
+	}
+	if (number >= base)
+		put_digit(number/base, base, len);
+	*len += write(1, &cipher[number], 1);
+}
+
