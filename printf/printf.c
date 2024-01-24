3009ec1a2bb9dae0ca96746f0457fc1a5a60c176
Martin Vokoun <mvokoun@c1r1s7.42prague.com>
2024-01-24 17:09:29 +0100

    up

--- printf/printf.c
+++ printf/printf.c
@@ -10,7 +10,7 @@ void put_string(char *str, int *len)
 		*len += write(1, str++, 1);
 }
 
-void put_digit(long int number, int base, int *len)
+void put_digit(long number, int base, int *len)
 {
 	char *cipher = "0123456789abcdef";
 	if(number < 0)
@@ -26,8 +26,8 @@ void put_digit(long int number, int base, int *len)
 int ft_printf(const char *string, ... )
 {
 	int ret_len = 0;
-	va_list pointer;
-	va_start (pointer, string);
+	va_list args;
+	va_start (args, string);		//void va_start(va_list ap, last_arg); ap: a_list object that will be initialized by va_start. last_arg: The last named parameter before the ellipsis (...) in the function declaration.
 
 	while (*string)
 	{
@@ -35,17 +35,17 @@ int ft_printf(const char *string, ... )
 		{
 			string += 1;
 			if(*string == 's')
-				put_string(va_arg(pointer, char *), &ret_len);		//After calling va_arg, the pointer is incremented to point to the next argument in the va_list. 
+				put_string(va_arg(args, char *), &ret_len);		//current argument is char *current = va_arg(args, char *), After calling va_arg, the args is incremented to point to the next argument in the va_list. 
 			else if (*string == 'd')
-				put_digit(va_arg(pointer, int), 10, &ret_len);
+				put_digit(va_arg(args, int), 10, &ret_len);
 			else if(*string == 'x')
-				put_digit(va_arg(pointer, int),16, &ret_len);
+				put_digit(va_arg(args, int),16, &ret_len);
 		}
 		else 
 			ret_len += write(1, string, 1);
 		string += 1;
 	}
-	return(va_end(pointer), ret_len);
+	return(va_end(args), ret_len);
 }
 
 int main()
@@ -55,5 +55,5 @@ int main()
 	int x = 1545311245;
 
 	ft_printf(" number: %d\n string: %s\n hexa: %x\n\n", i, s, x);
-	printf(" number: %d\n string: %s\n hexa: %x\n", i, s, x);
+	//printf(" number: %d\n string: %s\n hexa: %x\n", i, s, x);
 }
